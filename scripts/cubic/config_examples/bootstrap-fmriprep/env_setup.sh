28da474217f3038f6fbc68df7dd07008eb93b089
Dorota Jarecka <djarecka@gmail.com>
2022-04-04 10:45:49 -0400

    example of config for debbie

--- scripts/cubic/config_examples/bootstrap-fmriprep/env_setup.sh
+++ scripts/cubic/config_examples/bootstrap-fmriprep/env_setup.sh
@@ -1,4 +1,6 @@
 # Set up the correct conda environment
 module load openmind/singularity/3.5.0
 # assumin that there is a conda environment named datalad
-source /om2/user/djarecka/miniconda/bin/activate datalad                                                                                    
+source /om2/user/djarecka/miniconda/bin/activate datalad
+# setting freesurfer home
+export FREESURFER_HOME=/om2/user/smeisler/TheWay/scripts/mit_slurm
